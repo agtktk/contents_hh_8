3b435c624063541a17155dc50cc3e740299523a7
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:53:55 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: '정보이용료현금 희망티켓',
+  siteUrl: 'https://contents-hh-8.pages.dev',
+  siteDescription: '정보이용료현금 지급의 기준. 까다로운 한도 조회부터 최종 정산까지 365일 실시간으로 친절하게 안내합니다.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
